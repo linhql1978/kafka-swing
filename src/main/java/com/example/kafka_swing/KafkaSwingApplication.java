43714c2f6c7f69a889f437212b775fc3f326fa91
Hanson239 <hanhongson2309@gmail.com>
2020-09-13 13:30:17 +0700

    push

---
+++ src/main/java/com/example/kafka_swing/KafkaSwingApplication.java
@@ -0,0 +1,15 @@
+package com.example.kafka_swing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import view.GiaoDien;
+
+@SpringBootApplication
+public class KafkaSwingApplication {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(KafkaSwingApplication.class, args);
+    }
+
+}
