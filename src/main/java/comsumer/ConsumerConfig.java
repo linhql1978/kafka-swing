71ae1ed4cf21757d0d9e181f1e2889a57a5ee1a5
Quang Linh <linhql1978@gmail.com>
2020-09-17 17:13:34 +0700

    print info offset

--- src/main/java/comsumer/ConsumerConfig.java
+++ src/main/java/comsumer/ConsumerConfig.java
@@ -1,20 +1,27 @@
 package comsumer;
 
+import kafka.KafkaService;
+import org.apache.kafka.clients.KafkaClient;
+import org.apache.kafka.clients.admin.AdminClient;
+import org.apache.kafka.clients.admin.ListConsumerGroupOffsetsResult;
 import org.apache.kafka.clients.consumer.ConsumerRecords;
 import org.apache.kafka.clients.consumer.KafkaConsumer;
+import org.apache.kafka.clients.consumer.OffsetAndMetadata;
+import org.apache.kafka.common.TopicPartition;
 import org.apache.kafka.common.serialization.StringDeserializer;
 import view.App;
 
 import java.time.Duration;
-import java.util.Arrays;
-import java.util.Date;
-import java.util.Properties;
+import java.util.*;
+import java.util.concurrent.ExecutionException;
 
 
 public class ConsumerConfig {
+    private String group_id = String.valueOf(new Date().hashCode());
+
     public KafkaConsumer create() {
         String bootstrapServers = "13.76.157.231:9092,20.184.4.77:9092,104.42.73.42:9092";
-        String grp_id = String.valueOf((new Date().hashCode()));
+        String grp_id = String.valueOf(group_id);
         String topic = "stock-info";
 
         //Creating consumer properties
@@ -37,9 +44,30 @@ public class ConsumerConfig {
     }
 
     public void listen(KafkaConsumer consumer, App app) {
+        AdminClient adminClient = (new KafkaService()).getAdminClient();
+        ListConsumerGroupOffsetsResult listConsumerGroupOffsetsResult ;
+        long startEachMinute = System.currentTimeMillis();
+        long endEachMinute = System.currentTimeMillis();
         while (true) {
             ConsumerRecords<String, String> records = consumer.poll(Duration.ofMillis(0));
             app.reRender(records);
+            endEachMinute = System.currentTimeMillis();
+            if ((endEachMinute - startEachMinute) >= 15000) {
+                listConsumerGroupOffsetsResult = adminClient.listConsumerGroupOffsets(group_id);
+                try {
+                    Map<TopicPartition, OffsetAndMetadata> metadataMap = listConsumerGroupOffsetsResult.partitionsToOffsetAndMetadata().get();
+                    for (Map.Entry<TopicPartition, OffsetAndMetadata> entry : metadataMap.entrySet()) {
+                        System.out.println("topic: " + entry.getKey().topic() + ", partition: " + entry.getKey().partition() +
+                                ", offset: " + entry.getValue().offset() + ", endOffsets: " + consumer.endOffsets(Collections.singleton(entry.getKey())));
+                    }
+                    System.out.println("-------------------------");
+                    startEachMinute = endEachMinute;
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                } catch (ExecutionException e) {
+                    e.printStackTrace();
+                }
+            }
         }
     }
 }
