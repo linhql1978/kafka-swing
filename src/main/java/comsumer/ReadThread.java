1f8c7636022358f9d3afca546176204c61f1ad1c
thanbv1510 <thanbv1510@gmail.com>
2020-09-17 17:56:19 +0700

    top-n-price

--- src/main/java/comsumer/ReadThread.java
+++ src/main/java/comsumer/ReadThread.java
@@ -7,15 +7,17 @@ public class ReadThread extends Thread {
     private KafkaConsumer consumer;
     private ConsumerConfig consumerConfig;
     private App app;
+    private String topic;
 
-    public ReadThread(App app) {
+    public ReadThread(App app, String topic) {
         this.app = app;
         consumerConfig = new ConsumerConfig();
-        consumer = consumerConfig.create();
+        consumer = consumerConfig.create(topic);
+        this.topic = topic;
     }
 
     @Override
     public void run() {
-        consumerConfig.listen(consumer, app);
+        consumerConfig.listen(consumer, app, topic);
     }
 }
---
+++ src/main/java/model/SubTopNPriceModel.java
@@ -0,0 +1,49 @@
+package model;
+
+public class SubTopNPriceModel {
+    private String NumTopPrice;
+    private String BestBidPrice;
+    private String BestBidQtty;
+    private String BestOfferPrice;
+    private String BestOfferQtty;
+
+    public String getNumTopPrice() {
+        return NumTopPrice;
+    }
+
+    public void setNumTopPrice(String numTopPrice) {
+        NumTopPrice = numTopPrice;
+    }
+
+    public String getBestBidPrice() {
+        return BestBidPrice;
+    }
+
+    public void setBestBidPrice(String bestBidPrice) {
+        BestBidPrice = bestBidPrice;
+    }
+
+    public String getBestBidQtty() {
+        return BestBidQtty;
+    }
+
+    public void setBestBidQtty(String bestBidQtty) {
+        BestBidQtty = bestBidQtty;
+    }
+
+    public String getBestOfferPrice() {
+        return BestOfferPrice;
+    }
+
+    public void setBestOfferPrice(String bestOfferPrice) {
+        BestOfferPrice = bestOfferPrice;
+    }
+
+    public String getBestOfferQtty() {
+        return BestOfferQtty;
+    }
+
+    public void setBestOfferQtty(String bestOfferQtty) {
+        BestOfferQtty = bestOfferQtty;
+    }
+}
